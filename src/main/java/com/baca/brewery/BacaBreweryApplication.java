59bdca76752f410732fbcba328494fec01a6e3a2
199148 <199148@LTIN308862.cts.com>
2021-03-20 20:59:12 +0530

    intial commit

---
+++ src/main/java/com/baca/brewery/BacaBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.baca.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacaBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacaBreweryApplication.class, args);
+	}
+
+}
